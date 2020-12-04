6ed1fabc279335acda878b360aef51b4290e97b6
Christina Kurpiel <kurpielcm@gmail.com>
2020-12-03 21:16:24 -0500

    add new database info for transfer from mlab to Mongo Atlas

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,6 +1,6 @@
 // for dev
 module.exports = {
-    MONGODB_URI: 'mongodb://localhost/virtual-networking',
-    TOKEN_SECRET: 'bananas'
+    MONGODB_URI: 'mongodb+srv://christinak:7DwaW3mHL3hNVTd@muze.8lnbb.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    TOKEN_SECRET: '7DwaW3mHL3hNVTd'
   };
   
